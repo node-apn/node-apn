2ded1b9171e0687002e0674c68ef3e2b288ce48e
Andrew Naylor <argon@mkbot.net>
2010-08-01 12:27:02 +0100

    Added preliminary support for the enhanced binary protocol

--- node-apn.js
+++ node-apn.js
@@ -11,11 +11,13 @@ exports.create = function (optionArgs) {
 	var self = this;
 	var hasKey = hasCert = false;
 	
-	var options = 	{ cert: 'cert.pem'
-					, key:	'key.pem'
-					, gateway: 'gateway.push.apple.com'
-					, port: 2195
-					, enhanced: false
+	var options =	{ cert: 'cert.pem' /* Certificate file */
+					, key:	'key.pem'  /* Key file */
+					, gateway: 'gateway.push.apple.com' /* gateway address */
+					, port: 2195 /* gateway port */
+					, enhanced: false /* enable enhanced format */
+					, feedback: false /* enable feedback service, set to callback */
+					, feedbackInterval: 3600 /* interval in seconds to connect to feedback service */
 					};
 	
 	if (optionArgs) {
@@ -27,8 +29,8 @@ exports.create = function (optionArgs) {
 	}
 	
 	self.socket.on('connect', function() { console.log("connect."); });
-	self.socket.on('data', function(data) {  });
-	self.socket.on('end', function () { console.log('closed'); self.socket.end() });
+	self.socket.on('data', function(data) { /* Handle error data sent back */ });
+	self.socket.on('end', function () { console.log('closed'); self.socket.end(); });
 	
 	fs.readFile(options['cert'], function(err, data) {
 		if(err) {
@@ -57,17 +59,33 @@ exports.create = function (optionArgs) {
 		self.socket.setSecure(self.credentials);
 	}
 	
-	this.sendMessage = function (device, note) {
-		var hexTok = device.hexToken();
-		var message = JSON.stringify(note);
-		var data = new Buffer(hexTok.length + message.length + 5);
+	this.sendNotification = function (note) {
+		var hexTok = note.device.hexToken();
+		var message = JSON.stringify(note.payload);
 		var pos = 0;
 		
-		data[0] = 0;
-		pos = data.write(binLength(hexTok), 1, 'binary') + 1;
-		pos += data.write(hexTok, 3, 'binary');
-		pos += data.write(binLength(message), pos, 'binary');
-		data.write(message, pos);
+		if(options.enhanced) {
+			var data = new Buffer(1 + 4 + 4 + 2 + hexTok.length + 2 + message.length);
+			// Command
+			data[pos] = 1;
+			pos++;
+			
+			// Identifier
+			pos += data.write(int32val(note.identifier), pos, 'binary');
+			
+			// Expiry
+			pos += data.write(int32val(note.expiry), pos, 'binary');
+		}
+		else {
+			var data = new Buffer(1 + 2 + hexTok.length + 2 + message.length);
+			data[pos] = 0;
+			pos++;
+		}
+		
+		pos += data.write(int16val(hexTok.length), pos, 'binary');
+		pos += data.write(hexTok, pos, 'binary');
+		pos += data.write(int16val(message.length), pos, 'binary');
+		pos += data.write(message, pos);
 		
 		if(self.socket.readyState != 'open') {
 			if(self.socket.readyState == 'closed' && hasKey && hasCert) {
@@ -87,6 +105,9 @@ exports.create = function (optionArgs) {
 
 exports.notification = function () {
 	this.payload = {aps: {}};
+	this.expiry = 0;
+	this.identifier = 0;
+	this.device;
 }
 
 exports.device = function (token) {
@@ -110,8 +131,10 @@ exports.device = function (token) {
 	};
 }
 
-function binLength(string) {
-	var length = string.length;
-	var retVal = String.fromCharCode((length >> 8), (length & 0xff));
-	return retVal;
+function int16val(number) {
+	return String.fromCharCode(((number >> 8) & 0xff), (number & 0xff));
+}
+
+function int32val(number) {
+	return String.fromCharCode((number >> 24), ((number >> 16) & 0xff), ((number >> 8) & 0xff), (number & 0xfF));
 }
