node-apn/node-apn

commit a3cf8b090b673f96b155734221b42631619eea55

Fixes #16
Fix by @bobrik

lib/apn.js
@@ -48,7 +48,7 @@ var Connection = function (optionArgs) {
 	}
 
 	var onDrain = function() {
-		if (writeBuffer.length) {
+		while (writeBuffer.length && self.socket.bufferSize == 0) {
 			writeNotificationToSocket(writeBuffer.shift());
 		}
 	};

commit 53d04bae9289685f61383c07fedf32c7c95c4052

Fixes bug introduced by a3cf8b090b673f96b155734221b42631619eea55

lib/apn.js
@@ -51,7 +51,7 @@ var Connection = function (optionArgs) {
 	};
 
 	var onDrain = function () {
-		while (writeBuffer.length && self.socket.bufferSize == 0) {
+		while (writeBuffer.length && self.socket.socket.bufferSize == 0) {
 			writeNotificationToSocket(writeBuffer.shift());
 		}
 	};