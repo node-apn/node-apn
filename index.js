519c011bebf01cd017c4d047a69b2eea2b7dc960
Andrew Naylor <argon@mkbot.net>
2012-06-14 00:59:54 +0100

    Refactored connection handling.
    Now making extensive use of promises. Including the possibility of handling notification errors with a promise also.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./lib/apn');
+exports.Connection = require('./lib/connection');
+exports.connection = exports.Connection;
+
+exports.Device = require("./lib/device");
+exports.device = exports.Device;
+
+exports.Errors = require('./lib/errors');
+exports.error = exports.Errors;
+
+exports.Feedback = require("./lib/feedback");
+exports.feedback = exports.Feedback;
+
+exports.Notification = require("./lib/notification");
+exports.notification = exports.Notification;
