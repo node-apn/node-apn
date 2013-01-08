27438885dfb5c2d076fd752d2d8a838815337d78
HANAI Tohru <hanai@pokelabo.co.jp>
2013-01-08 21:03:44 +0900

    Changed notification architecutre; Added batch send, reduce EPIPE error.
    For end users:
    - Refreshed architecture; now it sends multiple notification in single
      transmission. And reduced EPIPE errors you may had seen.
    - Removed `Notification#device` and removed `Connection#sendNotification`.
    - Added `Connection#addNotification`.
    - Added `Connection#flush`.
    - Added `Connection#broadcast`. Which enables you to broadcast same
      notification content to many devices in a memory efficient way.
    - Added option `bucketLength` and `notificationWaitingTime`.
    
    For package developers:
    - Added feature of notification bucket, this enables you to send
      multiple notifications in a single transimission for traffic
      efficient delivery. Also it has come to prevent unexpectable EPIPE
      error; the error can be observed when some amount of data has been
      sent via a connection, > 8192 bytes. Now the connection reopen
      itself every time after the bucket went out.
    - Added connection state constants and variable and make
      `Connection#deferredConnection` exists always. This is required to
      determine connection available state in `Connection#run`.
    - Added `Connection#notificationWaitingTimer` to flush notifications
      automatically and periodically when the bucket is not full.
    - Removed `notification` argument from `Connection#raiseError`
      event. Since the effort won't reward to retrive notification from
      the bucket.
    - Disabled `Connection#disconnected` event.
    - Added tests, work with busterjs.

---
+++ test/test-connection.js
@@ -0,0 +1,102 @@
+var buster = require('buster');
+var Notification = require('../lib/notification');
+var Connection = require('../lib/connection');
+
+buster.testCase('Connection', {
+    setUp: function() {
+        this.connection = new Connection({notificationWaitingTime: 300});
+        this.stub(this.connection, 'connect', function() {
+            this.connection.deferredConnection.resolve();
+            this.connection.deferredConnectionWritable.resolve();
+            this.connection.connectionState = Connection.STATE_CONNECTED;
+            return this.connection.deferredConnection.promise;
+        }.bind(this));
+
+        this.stub(this.connection, 'isSocketWritable', function() {
+            return true;
+        });
+    },
+
+    'initial': function() {
+        var connection = new Connection();
+        assert.equals(connection.running, false);
+        assert.equals(connection.connectionState, Connection.STATE_DISCONNECTED);
+    },
+
+    'broadcast calls flush() right after callback returns false': function(done) {
+        var connection = this.connection;
+
+        var notification = new Notification();
+
+        // should be called three times.
+        var calledTimes = 0;
+        var callback = this.spy(function () {
+            switch (++calledTimes) {
+            case 1: return '0001';
+            case 2: return '0002';
+            case 3: return false;
+            }
+        });
+
+        // disable Connection#run
+        this.stub(connection, 'run');
+        
+        connection.broadcast(notification, callback);
+
+        connection.deferredBucketSendable.promise.then(function() {
+            assert(callback.calledThrice);
+            done();
+        });
+    },
+
+    'broadcast calls flush() when the bucket gets full': function(done) {
+        var connection = this.connection;
+
+        var notification = new Notification();
+        notification.alert = (new Array(200)).join('a')
+
+        var calledTimes = 0;
+        var flushed = false;
+        callback = this.spy(function () {
+            return flushed ? false : (new Array(65)).join('9')
+        });
+
+        // disable Connection#run
+        this.stub(connection, 'run');
+
+        connection.broadcast(notification, callback);
+
+        connection.deferredBucketSendable.promise.then(function() {
+            assert(true);
+            flushed = true;
+            done();
+        });
+
+    },
+
+    'addNotification': function() {
+        var connection = this.connection;
+
+        var notification = new Notification();
+        notification.alert = (new Array(200)).join('a')
+        var token = (new Array(65)).join('9');
+
+        // disable Connection#run
+        this.stub(connection, 'run');
+
+        var clock = this.useFakeTimers();
+
+        // add one
+        connection.addNotification(notification, token);
+
+        clock.tick(299);
+        // should not be flushed
+        assert(!connection.deferredBucketSendable.promise.isResolved());
+
+        clock.tick(1);
+        // should be flushed after elapsed 300ms 
+        assert(connection.deferredBucketSendable.promise.isResolved());
+
+        clock.restore();
+    }
+});
---
+++ test/test-notification-backet.js
@@ -0,0 +1,102 @@
+var buster = require('buster');
+var NotificationBucket = require('../lib/notification-bucket');
+var Notification = require('../lib/notification');
+
+buster.testCase('NotificationBucket', {
+    'initial values': function () {
+        var bucket = new NotificationBucket({});
+
+        assert(bucket.getBuffer() instanceof Buffer);
+        assert(bucket.options.enhanced);
+        assert.equals(bucket.notificationCount, 0);
+        assert(0 < bucket.options.maxLength);
+        assert.equals(bucket.availableLength(), bucket.options.maxLength);
+        assert(0 < bucket.sentinelNotificationLength());
+    },
+
+    'sentinel': function() {
+        var bucket = new NotificationBucket({});
+
+        bucket.appendSentinelNotification(1);
+        assert.equals(bucket.availableLength(), bucket.options.maxLength - bucket.sentinelNotificationLength());
+
+        // bucket.notificationCount shouldn't count the sentinel
+        assert.equals(bucket.notificationCount, 0);
+    },
+
+    'notification': function() {
+        var bucket = new NotificationBucket({});
+
+        // append an almost empty notification
+        var emptyNotification = new Notification();
+        emptyNotification.alert = '';
+        var templateEmpty = emptyNotification.toBinaryTemplate();
+        var token = new Buffer(0);
+
+        var expectedLength = 1 + 4 + 4 + 2 + token.length + 2 + templateEmpty.payload.length;
+        assert.equals(bucket.calculateNotificationLength(templateEmpty, token), expectedLength);
+                      
+        bucket.appendToBuffer(templateEmpty, token, 1);
+        assert.equals(bucket.availableLength(), bucket.options.maxLength - expectedLength);
+        assert.equals(bucket.notificationCount, 1);
+
+        // append another
+        var notification1 = new Notification();
+        notification1.alert = 'abc';
+        var template1 = notification1.toBinaryTemplate();
+        assert.equals(template1.payload.length, templateEmpty.payload.length + 3);
+        var token1 = new Buffer(8);
+
+        var availableLength = bucket.availableLength();
+        var expectedLength1 = 1 + 4 + 4 + 2 + token1.length + 2 + template1.payload.length;
+        assert.equals(bucket.calculateNotificationLength(template1, token1), expectedLength1);
+        bucket.appendToBuffer(template1, token1, 2);
+        assert.equals(bucket.availableLength(), availableLength - expectedLength1);
+        assert.equals(bucket.notificationCount, 2);
+
+        // make the buffer full (append sentinel to its tail)
+        var id = 3;
+        while (expectedLength1 * 2 < bucket.availableLength()) {
+            bucket.appendToBuffer(template1, token1, id++);
+        }
+        var lastTokenLength = token1.length + (bucket.availableLength() - expectedLength1) -
+            bucket.sentinelNotificationLength();
+        var token2 = new Buffer(lastTokenLength);
+        bucket.appendToBuffer(template1, token2, id);
+        var lastId = id;
+
+        bucket.appendSentinelNotification(9999);
+        assert.equals(bucket.availableLength(), 0);
+
+        // purge
+        var currentCount = bucket.notificationCount;
+
+        // purge the head
+        assert.equals(bucket.purgeNotificationUntil(1), 1);
+        --currentCount;
+        assert.equals(bucket.notificationCount, currentCount);
+
+        // that should have removed the sentinel implicitly
+        assert.equals(bucket.availableLength(),
+                      expectedLength + bucket.sentinelNotificationLength());
+
+        // purge for unknown id should do nothing
+        assert.same(bucket.purgeNotificationUntil(998), false);
+
+        // purge next
+        assert.equals(bucket.purgeNotificationUntil(2), 1);
+        --currentCount;
+
+        // purge for tail
+        assert.equals(bucket.purgeNotificationUntil(lastId), currentCount);
+        assert.equals(bucket.notificationCount, 0);
+        assert.equals(bucket.availableLength(), bucket.options.maxLength);
+
+        // clear notification
+        bucket.appendToBuffer(template1, token2, id);
+        bucket.clear();
+        assert.equals(bucket.notificationCount, 0);
+        assert.equals(bucket.availableLength(), bucket.options.maxLength);
+        bucket.clear();         // clear for empty buffer should make no effect
+    }
+});
