97bbc811e951d2a6579ad7badaf0ef8ccd44c4bd
Andrew Naylor <argon@mkbot.net>
2014-04-21 23:43:23 +0100

    Moved test system over to chai with chai-as-promised
    Implemented several tests, including coverage for new credential loading system in 1.5.0.

--- test/feedback.js
+++ test/feedback.js
@@ -1,37 +1,47 @@
 var apn = require("../");
 var fs = require("fs");
 
-describe("Connection", function() {
+describe("Feedback", function() {
 	describe('constructor', function () {
+		var requestMethod;
+
+		before(function() {
+			requestMethod = apn.Feedback.prototype.request;
+			apn.Feedback.prototype.request = function() { };
+		});
+
+		after(function() {
+			apn.Feedback.prototype.request = requestMethod;
+		})
 
 		// Issue #50
-		it("should use gateway.sandbox.push.apple.com as the default connection address", function () {
+		it("should use feedback.sandbox.push.apple.com as the default Feedback address", function () {
 			var existingEnv = process.env.NODE_ENV;
 			process.env.NODE_ENV = "";
-			apn.Connection().options.address.should.equal("gateway.sandbox.push.apple.com");
+			apn.Feedback().options.address.should.equal("feedback.sandbox.push.apple.com");
 			process.env.NODE_ENV = existingEnv;
 		});
 
-		it("should use gateway.push.apple.com when NODE_ENV=production", function () {
+		it("should use feedback.push.apple.com when NODE_ENV=production", function () {
 			var existingEnv = process.env.NODE_ENV;
 			process.env.NODE_ENV = "production";
-			apn.Connection().options.address.should.equal("gateway.push.apple.com");
+			apn.Feedback().options.address.should.equal("feedback.push.apple.com");
 			process.env.NODE_ENV = existingEnv;
+		});
+
+		it("should use feedback.push.apple.com when production:true", function () {
+			apn.Feedback({production:true}).options.address.should.equal("feedback.push.apple.com");
 		});
 
 		it("should give precedence to production flag over NODE_ENV=production", function () {
 			var existingEnv = process.env.NODE_ENV;
 			process.env.NODE_ENV = "production";
-			apn.Connection({ production: false }).options.address.should.equal("gateway.sandbox.push.apple.com");
+			apn.Feedback({ production: false }).options.address.should.equal("feedback.sandbox.push.apple.com");
 			process.env.NODE_ENV = existingEnv;
 		});
 
-		it("should use gateway.push.apple.com when production:true", function () {
-			apn.Connection({production:true}).options.address.should.equal("gateway.push.apple.com");
-		});
-
 		it("should use a custom address when passed", function () {
-			apn.Connection({address: "testaddress"}).options.address.should.equal("testaddress");
+			apn.Feedback({address: "testaddress"}).options.address.should.equal("testaddress");
 		});
 	});
 
@@ -44,89 +54,89 @@ describe("Connection", function() {
 		});
 
 		it("should eventually load a pfx file from disk", function () {
-			return apn.Connection({ pfx: "test/support/initializeTest.pfx" })
+			return apn.Feedback({ pfx: "test/support/initializeTest.pfx" })
 					  .initialize().get(0).post("toString")
 					  .should.eventually.equal(pfx.toString());
 		});
 
 		it("should eventually provide pfx data from memory", function () {
-			return apn.Connection({ pfx: pfx }).initialize().get(0).post("toString")
+			return apn.Feedback({ pfx: pfx }).initialize().get(0).post("toString")
 					  .should.eventually.equal(pfx.toString());
 		});
 
 		it("should eventually provide pfx data explicitly passed in pfxData parameter", function () {
-			return apn.Connection({ pfxData: pfx }).initialize().get(0).post("toString")
+			return apn.Feedback({ pfxData: pfx }).initialize().get(0).post("toString")
 					  .should.eventually.equal(pfx.toString());
 		});
 
 		it("should eventually load a certificate from disk", function () {
-			return apn.Connection({ cert: "test/support/initializeTest.crt", key: null})
+			return apn.Feedback({ cert: "test/support/initializeTest.crt", key: null})
 					  .initialize().get(1).post("toString")
 					  .should.eventually.equal(cert.toString());
 		});
 
 		it("should eventually provide a certificate from a Buffer", function () {
-			return apn.Connection({ cert: cert, key: null})
+			return apn.Feedback({ cert: cert, key: null})
 					  .initialize().get(1).post("toString")
 					  .should.eventually.equal(cert.toString());
 		});
 
 		it("should eventually provide a certificate from a String", function () {
-			return apn.Connection({ cert: cert.toString(), key: null})
+			return apn.Feedback({ cert: cert.toString(), key: null})
 					  .initialize().get(1)
 					  .should.eventually.equal(cert.toString());
 		});
 
 		it("should eventually provide certificate data explicitly passed in the certData parameter", function () {
-			return apn.Connection({ certData: cert, key: null})
+			return apn.Feedback({ certData: cert, key: null})
 					  .initialize().get(1).post("toString")
 					  .should.eventually.equal(cert.toString());
 		});
 
 		it("should eventually load a key from disk", function () {
-			return apn.Connection({ cert: null, key: "test/support/initializeTest.key"})
+			return apn.Feedback({ cert: null, key: "test/support/initializeTest.key"})
 					  .initialize().get(2).post("toString")
 					  .should.eventually.equal(key.toString());
 		});
 
 		it("should eventually provide a key from a Buffer", function () {
-			return apn.Connection({ cert: null, key: key})
+			return apn.Feedback({ cert: null, key: key})
 					  .initialize().get(2).post("toString")
 					  .should.eventually.equal(key.toString());
 		});
 
 		it("should eventually provide a key from a String", function () {
-			return apn.Connection({ cert: null, key: key.toString()})
+			return apn.Feedback({ cert: null, key: key.toString()})
 					  .initialize().get(2)
 					  .should.eventually.equal(key.toString());
 		})
 
 		it("should eventually provide key data explicitly passed in the keyData parameter", function () {
-			return apn.Connection({ cert: null, keyData: key})
+			return apn.Feedback({ cert: null, keyData: key})
 					  .initialize().get(2).post("toString")
 					  .should.eventually.equal(key.toString());
 		});
 
 		it("should eventually load a single CA certificate from disk", function () {
-			return apn.Connection({ cert: null, key: null, ca: "test/support/initializeTest.crt" })
+			return apn.Feedback({ cert: null, key: null, ca: "test/support/initializeTest.crt" })
 					  .initialize().get(3).get(0).post("toString")
 					  .should.eventually.equal(cert.toString());
 		});
 
 		it("should eventually provide a single CA certificate from a Buffer", function () {
-			return apn.Connection({ cert: null, key: null, ca: cert })
+			return apn.Feedback({ cert: null, key: null, ca: cert })
 					  .initialize().get(3).get(0).post("toString")
 					  .should.eventually.equal(cert.toString());
 		});
 
 		it("should eventually provide a single CA certificate from a String", function () {
-			return apn.Connection({ cert: null, key: null, ca: cert.toString() })
+			return apn.Feedback({ cert: null, key: null, ca: cert.toString() })
 					  .initialize().get(3).get(0)
 					  .should.eventually.equal(cert.toString());
 		});
 
 		it("should eventually load an array of CA certificates", function (done) {
-			apn.Connection({ cert: null, key: null, ca: ["test/support/initializeTest.crt", cert, cert.toString()] })
+			apn.Feedback({ cert: null, key: null, ca: ["test/support/initializeTest.crt", cert, cert.toString()] })
 			  .initialize().get(3).spread(function(cert1, cert2, cert3) {
 			  	var certString = cert.toString();
 			  	if (cert1.toString() == certString && 
---
+++ test/support.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.should();
+chai.use(chaiAsPromised);
