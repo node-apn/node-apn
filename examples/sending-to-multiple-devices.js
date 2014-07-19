ce4aa7c521a5dfd4b43817f0c78be36bea215293
Andrew Naylor <argon@mkbot.net>
2014-07-19 21:45:27 +0100

    Improvements to the clarity of the sample code.

--- examples/sending-to-multiple-devices.js
+++ examples/sending-to-multiple-devices.js
@@ -21,6 +21,9 @@ service.on('transmitted', function(notification, device) {
 
 service.on('transmissionError', function(errCode, notification, device) {
     console.error("Notification caused error: " + errCode + " for device ", device, notification);
+    if (errCode == 8) {
+        console.log("A error code of 8 indicates that the device token is invalid. This could be for a number of reasons - are you using the correct environment? i.e. Production vs. Sandbox");
+    }
 });
 
 service.on('timeout', function () {
@@ -36,6 +39,7 @@ service.on('socketError', console.error);
 
 // If you plan on sending identical paylods to many devices you can do something like this.
 function pushNotificationToMany() {
+    console.log("Sending the same notification each of the devices with one call to pushNotification.");
     var note = new apn.notification();
     note.setAlertText("Hello, from node-apn!");
     note.badge = 1;
@@ -48,6 +52,7 @@ pushNotificationToMany();
 
 // If you have a list of devices for which you want to send a customised notification you can create one and send it to and individual device.
 function pushSomeNotifications() {
+    console.log("Sending a tailored notification to %d devices", tokens.length);
     for (var i in tokens) {
         var note = new apn.notification();
         note.setAlertText("Hello, from node-apn! You are number: " + i);
