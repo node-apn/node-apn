d5a29c17332b3c8e450fe741c9fdf2b7a4fe2fd8
Andrew Naylor <argon@mkbot.net>
2015-01-12 19:53:20 +0000

    Decode all certificates from PEM

--- test/credentials/apnCertificateFromPem.js
+++ test/credentials/apnCertificateFromPem.js
@@ -13,30 +13,68 @@ describe("apnCertificateFromPem", function() {
 			certProperties = apnCertificateFromPem(cert);
 		});
 
-		it("returns an APNCertificate", function() {
-			expect(certProperties).to.be.an.instanceof(APNCertificate);
+		describe("return value", function() {
+			it("is an array", function() {
+				expect(certProperties).to.be.an('array');
+			});
+
+			it("contains one element", function() {
+				expect(certProperties).to.have.length(1);
+			});
+
+			describe("certificate [0]", function() {
+				it("is an APNCertificate", function() {
+					expect(certProperties[0]).to.be.an.instanceof(APNCertificate);
+				});
+
+				it("has the correct fingerprint", function() {
+					expect(certProperties[0].key().fingerprint()).to.equal("2d594c9861227dd22ba5ae37cc9354e9117a804d");
+				});
+			});
+		});
+	});
+
+	describe("with PEM containing multiple certificates", function() {
+		var cert, certProperties;
+		before(function() {
+			cert = fs.readFileSync("test/credentials/support/certIssuerKey.pem");
 		});
 
-		it("returns correct certificate data", function() {
-			expect(certProperties.key().fingerprint()).to.equal("2d594c9861227dd22ba5ae37cc9354e9117a804d");
-		});
-	})
-
-	describe("throws", function() {
-		it("for a PEM key", function() {
-			var key = fs.readFileSync("test/credentials/support/key.pem");
-			expect(function() {
-				apnCertificateFromPem(key);
-			}).to.throw(Error);
+		beforeEach(function() {
+			certProperties = apnCertificateFromPem(cert);
 		});
 
-		it("for a PKCS#12 file", function() {
+		it("returns the correct number of certificates", function() {
+			expect(certProperties).to.have.length(2);
+		});
+
+		describe("certificate [0]", function() {
+			it("has the correct fingerprint", function() {
+				expect(certProperties[0].key().fingerprint()).to.equal("2d594c9861227dd22ba5ae37cc9354e9117a804d");
+			});
+		});
+
+		describe("certificate [1]", function() {
+			it("has the correct fingerprint", function() {
+				expect(certProperties[1].key().fingerprint()).to.equal("ccff221d67cb3335649f9b4fbb311948af76f4b2");
+			});
+		});
+	});
+
+	describe("with a PKCS#12 file", function() {
+		it("throws", function() {
 			var pfx = fs.readFileSync("test/credentials/support/test.p12");
 			expect(function() {
 				apnCertificateFromPem(pfx);
-			}).to.throw(Error);
+			}).to.throw("unable to load certificate, not a valid PEM file");
 		});
+	});
 
+	describe("with a key", function() {
+		it("returns an empty array", function() {
+			var key = fs.readFileSync("test/credentials/support/key.pem");
+			expect(apnCertificateFromPem(key)).to.be.empty;
+		});
 	});
 	
 	describe("returns null", function() {
