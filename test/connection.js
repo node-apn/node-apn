44b3f031253ae0bf98ea1f12a44ae1b73955445e
Andrew Naylor <argon@mkbot.net>
2015-01-30 08:19:24 +0000

    Improved tests to aid next additions

--- test/connection.js
+++ test/connection.js
@@ -1,4 +1,6 @@
-var apn = require("../");
+var rewire = require("rewire");
+var Connection = rewire("../lib/connection");
+
 var fs = require("fs");
 
 describe("Connection", function() {
@@ -19,31 +21,31 @@ describe("Connection", function() {
 
 		// Issue #50
 		it("should use gateway.sandbox.push.apple.com as the default connection address", function () {
-			expect(apn.Connection().options.address).to.equal("gateway.sandbox.push.apple.com");
+			expect(Connection().options.address).to.equal("gateway.sandbox.push.apple.com");
 		});
 
 		it("should use gateway.push.apple.com when NODE_ENV=production", function () {
 			process.env.NODE_ENV = "production";
-			expect(apn.Connection().options.address).to.equal("gateway.push.apple.com");
+			expect(Connection().options.address).to.equal("gateway.push.apple.com");
 		});
 
 		it("should give precedence to production flag over NODE_ENV=production", function () {
 			process.env.NODE_ENV = "production";
-			expect(apn.Connection({ production: false }).options.address).to.equal("gateway.sandbox.push.apple.com");
+			expect(Connection({ production: false }).options.address).to.equal("gateway.sandbox.push.apple.com");
 		});
 
 		it("should use gateway.push.apple.com when production:true", function () {
-			expect(apn.Connection({production:true}).options.address).to.equal("gateway.push.apple.com");
+			expect(Connection({production:true}).options.address).to.equal("gateway.push.apple.com");
 		});
 
 		it("should use a custom address when passed", function () {
-			expect(apn.Connection({address: "testaddress"}).options.address).to.equal("testaddress");
+			expect(Connection({address: "testaddress"}).options.address).to.equal("testaddress");
 		});
 	});
 
 	describe('#initialize', function () {
 		it("should be fulfilled", function () {
-			return expect(apn.Connection({ pfx: "test/support/initializeTest.pfx" })
+			return expect(Connection({ pfx: "test/support/initializeTest.pfx" })
 					  .initialize()).to.be.fulfilled;
 		});
 	});
--- test/feedback.js
+++ test/feedback.js
@@ -1,19 +1,27 @@
-var apn = require("../");
-var fs = require("fs");
+var rewire = require("rewire");
+var Feedback = rewire("../lib/feedback");
+
 
 describe("Feedback", function() {
+	var startMethod;
+	before(function() {
+		// Constructor has side effects :-(
+		startMethod = Feedback.prototype.start;
+		Feedback.prototype.start = function() { };
+	});
+
+	after(function() {
+		Feedback.prototype.start = startMethod;
+	});
+
 	describe('constructor', function () {
-		var requestMethod, originalEnv;
+		var originalEnv;
 
 		before(function() {
-			requestMethod = apn.Feedback.prototype.request;
-			apn.Feedback.prototype.request = function() { };
-
 			originalEnv = process.env.NODE_ENV;
 		});
 
 		after(function() {
-			apn.Feedback.prototype.request = requestMethod;
 			process.env.NODE_ENV = originalEnv;
 		});
 
@@ -23,32 +31,31 @@ describe("Feedback", function() {
 
 		// Issue #50
 		it("should use feedback.sandbox.push.apple.com as the default Feedback address", function () {
-			expect(apn.Feedback().options.address).to.equal("feedback.sandbox.push.apple.com");
+			expect(Feedback().options.address).to.equal("feedback.sandbox.push.apple.com");
 		});
 
 		it("should use feedback.push.apple.com when NODE_ENV=production", function () {
 			process.env.NODE_ENV = "production";
-			expect(apn.Feedback().options.address).to.equal("feedback.push.apple.com");
+			expect(Feedback().options.address).to.equal("feedback.push.apple.com");
 		});
 
 		it("should use feedback.push.apple.com when production:true", function () {
-			expect(apn.Feedback({production:true}).options.address).to.equal("feedback.push.apple.com");
+			expect(Feedback({production:true}).options.address).to.equal("feedback.push.apple.com");
 		});
 
 		it("should give precedence to production flag over NODE_ENV=production", function () {
 			process.env.NODE_ENV = "production";
-			expect(apn.Feedback({ production: false }).options.address).to.equal("feedback.sandbox.push.apple.com");
+			expect(Feedback({ production: false }).options.address).to.equal("feedback.sandbox.push.apple.com");
 		});
 
 		it("should use a custom address when passed", function () {
-			expect(apn.Feedback({address: "testaddress"}).options.address).to.equal("testaddress");
+			expect(Feedback({address: "testaddress"}).options.address).to.equal("testaddress");
 		});
 	});
 
 	describe('#initialize', function () {
-
 		it("should be fulfilled", function () {
-			return expect(apn.Feedback({ pfx: "test/support/initializeTest.pfx" })
+			return expect(Feedback({ pfx: "test/support/initializeTest.pfx" })
 					  .initialize()).to.be.fulfilled;
 		});
 	});
