37da13c6d61a8d0be239b9f6c6c054ef5aece987
Andrew Naylor <argon@mkbot.net>
2015-02-05 00:14:29 +0000

    Simplified test setup/teardown

--- test/credentials/validate.js
+++ test/credentials/validate.js
@@ -2,118 +2,110 @@ var sinon = require("sinon");
 var validateCredentials = require("../../lib/credentials/validate");
 
 describe("validateCredentials", function() {
+	var credentials;
+	beforeEach(function() {
+		credentials = fakeCredentials();
+	});
+
 	describe("with valid credentials", function() {
 		it("returns", function() {
 			expect(function() {
-				validateCredentials(fakeCredentials);
+				validateCredentials(credentials);
 			}).to.not.throw();
 		});
 	});
 
 	describe("with mismatched key and certificate", function() {
 		it("throws", function() {
-			sinon.stub(fakeCredentials.certificates[0]._key, "fingerprint").returns("fingerprint2");
+			sinon.stub(credentials.certificates[0]._key, "fingerprint").returns("fingerprint2");
 			
 			expect(function() {
-				validateCredentials(fakeCredentials);
+				validateCredentials(credentials);
 			}).to.throw(/certificate and key do not match/);
-
-			fakeCredentials.certificates[0]._key.fingerprint.restore();
 		});
 	});
 
 	describe("with expired certificate", function() {
 		it("throws", function() {
-			sinon.stub(fakeCredentials.certificates[0], "validity")
+			sinon.stub(credentials.certificates[0], "validity")
 				.returns({
 					notBefore: new Date(Date.now() - 100000),
 					notAfter: new Date(Date.now() - 10000)
 				});
 
 			expect(function() {
-				validateCredentials(fakeCredentials);
+				validateCredentials(credentials);
 			}).to.throw(/certificate has expired/);
-
-			fakeCredentials.certificates[0].validity.restore();
 		});
 	});
 
 	describe("with incorrect environment", function() {
-		afterEach(function() {
-			fakeCredentials.certificates[0].environment.restore();
-		});
-
 		it("throws with sandbox cert in production", function() {
-			sinon.stub(fakeCredentials.certificates[0], "environment")
+			sinon.stub(credentials.certificates[0], "environment")
 				.returns({
 					production: false,
 					sandbox: true
 				});
 
 			expect(function() {
-				validateCredentials(fakeCredentials);
+				validateCredentials(credentials);
 			}).to.throw("certificate does not support configured environment, production: true");
 		});
 
 		it("throws with production cert in sandbox", function() {
-			sinon.stub(fakeCredentials.certificates[0], "environment")
+			sinon.stub(credentials.certificates[0], "environment")
 				.returns({
 					production: true,
 					sandbox: false
 				});
-			fakeCredentials.production = false;
+			credentials.production = false;
 
 			expect(function() {
-				validateCredentials(fakeCredentials);
+				validateCredentials(credentials);
 			}).to.throw("certificate does not support configured environment, production: false");
 
-			fakeCredentials.production = true;
 		});
 	});
 
 	describe("with certificate supporting both environments", function() {
-		afterEach(function() {
-			fakeCredentials.certificates[0].environment.restore();
-		});
-		
 		it("does not throw", function() {
-			sinon.stub(fakeCredentials.certificates[0], "environment")
+			sinon.stub(credentials.certificates[0], "environment")
 				.returns({
 					production: true,
 					sandbox: true
 				});
-			fakeCredentials.production = false;
+			credentials.production = false;
 
 			expect(function() {
-				validateCredentials(fakeCredentials);
+				validateCredentials(credentials);
 			}).to.not.throw();
-
-			fakeCredentials.production = true;
 		});
 	});
 });
 
-var fakeCredentials = {
-	key: {
-		_fingerprint: "fingerprint1",
-		fingerprint: function() { return this._fingerprint; },
-	},
-	certificates: [{
-		_key: {
+var fakeCredentials = function() {
+	return {
+		key: {
 			_fingerprint: "fingerprint1",
 			fingerprint: function() { return this._fingerprint; },
 		},
-		_validity: {
-			notBefore: new Date(Date.now() - 100000),
-			notAfter: new Date(Date.now() + 100000)
-		},
-		key: function() { return this._key; },
-		validity: function() {
-			return this._validity;
-		},
-		environment: function() {
-			return { production: true, sandbox: false };
-		}
-	}],
-	production: true
+		certificates: [{
+			_key: {
+				_fingerprint: "fingerprint1",
+				fingerprint: function() { return this._fingerprint; },
+			},
+			_validity: {
+				notBefore: new Date(Date.now() - 100000),
+				notAfter: new Date(Date.now() + 100000)
+			},
+			key: function() { return this._key; },
+			validity: function() {
+				return this._validity;
+			},
+			environment: function() {
+				return { production: true, sandbox: false };
+			}
+		}],
+		production: true
+	};
 };
