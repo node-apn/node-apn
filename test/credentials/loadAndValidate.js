3d1a98a6e1c79a5927aad7010eb75b9b12da14fe
Andrew Naylor <argon@mkbot.net>
2016-02-28 23:50:00 +0000

    Move credential loading to its own function

---
+++ test/credentials/loadAndValidate.js
@@ -0,0 +1,143 @@
+"use strict";
+
+const sinon = require("sinon");
+
+describe("loadAndValidate", function () {
+  let fakes, loadAndValidate;
+
+  beforeEach(() => {
+    fakes = {
+      load: sinon.stub(),
+      parse: sinon.stub(),
+      validate: sinon.stub(),
+    }
+
+    loadAndValidate = require("../../lib/credentials/loadAndValidate")(fakes);
+  });
+
+  describe("with valid credentials", function() {
+    let credentials;
+    const testOptions = {
+      pfx: "myCredentials.pfx",
+      cert: "myCert.pem",
+      key: "myKey.pem",
+      ca: "myCa.pem",
+      passphrase: "apntest",
+      production: true,
+    };
+
+    beforeEach(function() {
+      fakes.load.withArgs(sinon.match(testOptions)).returns(
+        {
+          pfx: "myPfxData",
+          cert: "myCertData",
+          key: "myKeyData",
+          ca: ["myCaData"],
+          passphrase: "apntest",
+        }
+      );
+
+      fakes.parse.returnsArg(0);
+      credentials = loadAndValidate(testOptions);
+    });
+
+    describe("the validation stage", function() {
+      it("is called once", function() {
+        expect(fakes.validate).to.be.calledOnce;
+      });
+
+      it("is passed the production flag", function() {
+        expect(fakes.validate.getCall(0).args[0]).to.have.property("production", true);
+      });
+
+      describe("passed credentials", function() {
+        it("contains the PFX data", function() {
+          expect(fakes.validate.getCall(0).args[0]).to.have.property("pfx", "myPfxData");
+        });
+
+        it("contains the key data", function() {
+          expect(fakes.validate.getCall(0).args[0]).to.have.property("key", "myKeyData");
+        });
+
+        it("contains the certificate data", function() {
+          expect(fakes.validate.getCall(0).args[0]).to.have.property("cert", "myCertData");
+        });
+
+        it("includes passphrase", function() {
+          expect(fakes.validate.getCall(0).args[0]).to.have.property("passphrase", "apntest");
+        });
+      });
+    });
+
+    describe("resolution value", function() {
+
+      it("contains the PFX data", function() {
+        return expect(credentials).to.have.property("pfx", "myPfxData");
+      });
+
+      it("contains the key data", function() {
+        return expect(credentials).to.have.property("key", "myKeyData");
+      });
+
+      it("contains the certificate data", function() {
+        return expect(credentials).to.have.property("cert", "myCertData");
+      });
+
+      it("contains the CA data", function() {
+        return expect(credentials).to.have.deep.property("ca[0]", "myCaData");
+      });
+
+      it("includes passphrase", function() {
+        return expect(credentials).to.have.property("passphrase", "apntest");
+      });
+    });
+  });
+
+  describe("credential file cannot be parsed", function() {
+    beforeEach(function() {
+      fakes.load.returns({ cert: "myCertData", key: "myKeyData" });
+      fakes.parse.throws(new Error("unable to parse key"));
+    });
+
+    it("should resolve with the credentials", function() {
+      let credentials = loadAndValidate({ cert: "myUnparseableCert.pem", key: "myUnparseableKey.pem", production: true });
+      return expect(credentials).to.deep.equal({ cert: "myCertData", key: "myKeyData" });
+    });
+
+    xit("should log an error", function() {
+      var debug = sinon.spy();
+      var reset = Connection.__set__("debug", debug);
+      var credentials = Connection({ cert: "myUnparseableCert.pem", key: "myUnparseableKey.pem" }).loadCredentials();
+
+      return credentials.finally(function() {
+        reset();
+        expect(debug).to.be.calledWith(sinon.match(function(err) {
+          return err.message ? err.message.match(/unable to parse key/) : false;
+        }, "\"unable to parse key\""));
+      });
+    });
+
+    it("should not attempt to validate", function() {
+      let credentials = loadAndValidate({ cert: "myUnparseableCert.pem", key: "myUnparseableKey.pem" });
+      expect(fakes.validate).to.not.be.called;
+    });
+  });
+
+  describe("credential validation fails", function() {
+    it("should throw", function() {
+      fakes.load.returns(Promise.resolve({ cert: "myCertData", key: "myMismatchedKeyData" }));
+      fakes.parse.returnsArg(0);
+      fakes.validate.throws(new Error("certificate and key do not match"));
+
+      return expect(() => loadAndValidate({ cert: "myCert.pem", key: "myMistmatchedKey.pem" })).to.throw(/certificate and key do not match/);
+    });
+  });
+
+  describe("credential file cannot be loaded", function() {
+    it("should throw", function() {
+      fakes.load.throws(new Error("ENOENT, no such file or directory"));
+
+      return expect(() => loadAndValidate({ cert: "noSuchFile.pem", key: "myKey.pem" })).to.throw("ENOENT, no such file or directory");
+    });
+  });
+});
