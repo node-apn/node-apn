e4f2b2ff50aa467c671893f5f02119b1e2509b7d
Andrew Naylor <argon@mkbot.net>
2016-02-29 01:37:09 +0000

    Move the configuration to its own function

---
+++ test/config.js
@@ -0,0 +1,154 @@
+"use strict";
+
+const sinon = require("sinon");
+
+describe("config", () => {
+  let config, fakes;
+
+  beforeEach(function() {
+    fakes = {
+      debug: sinon.spy()
+    };
+
+    config = require("../lib/config")(fakes);
+  });
+
+  it("supplies sensible defaults", () => {
+    expect(config()).to.deep.equal({
+      cert: "cert.pem",
+			key: "key.pem",
+			ca: null,
+			pfx: null,
+			passphrase: null,
+			production: false,
+			address: "api.sandbox.push.apple.com",
+			rejectUnauthorized: true,
+			connectTimeout: 10000,
+			connectionTimeout: 3600000,
+			connectionRetryLimit: 10,
+    });
+  });
+
+  describe("address configuration", () => {
+    let originalEnv;
+
+    before(function() {
+      originalEnv = process.env.NODE_ENV;
+    });
+
+    after(function() {
+      process.env.NODE_ENV = originalEnv;
+    });
+
+    beforeEach(function() {
+      process.env.NODE_ENV = "";
+    });
+
+    it("should use api.sandbox.push.apple.com as the default connection address", function () {
+      expect(config()).to.have.property("address", "api.sandbox.push.apple.com");
+    });
+
+    it("should use api.push.apple.com when NODE_ENV=production", function () {
+      process.env.NODE_ENV = "production";
+      expect(config()).to.have.property("address", "api.push.apple.com");
+    });
+
+    it("should give precedence to production flag over NODE_ENV=production", function () {
+      process.env.NODE_ENV = "production";
+      expect(config({ production: false })).to.have.property("address", "api.sandbox.push.apple.com");
+    });
+
+    it("should use api.push.apple.com when production:true", function () {
+      expect(config({production:true})).to.have.property("address", "api.push.apple.com");
+    });
+
+    it("should use a custom address when passed", function () {
+      expect(config({address: "testaddress"})).to.have.property("address", "testaddress");
+    });
+
+    describe("address is passed", function() {
+      it("sets production to true when using production address", function() {
+        expect(config({address: "api.push.apple.com"})).to.have.property("production", true);
+      });
+
+      it("sets production to false when using sandbox address", function() {
+        process.env.NODE_ENV = "production";
+        expect(config({address: "api.sandbox.push.apple.com"})).to.have.property("production", false);
+      });
+    });
+  });
+
+  describe("credential defaults", () => {
+    context("pfx value is supplied without cert and key", () => {
+      it("includes the value of `pfx`", () => {
+        expect(config( { pfx: "apn.pfx" } )).to.have.property("pfx", "apn.pfx");
+      });
+
+      it("does not include a value for `cert`", () => {
+        expect(config( { pfx: "apn.pfx" }).cert).to.be.undefined;
+      });
+
+      it("does not include a value for `key`", () => {
+        expect(config( { pfx: "apn.pfx" }).key).to.be.undefined;
+      });
+    });
+
+    context("pfx value is supplied along with a cert and key", () => {
+      it("includes the value of `pfx`", () => {
+        expect(config( { pfx: "apn.pfx", cert: "cert.pem", key: "key.pem" } )).to.have.property("pfx", "apn.pfx");
+      });
+
+      it("does not include a value for `cert`", () => {
+        expect(config( { pfx: "apn.pfx", cert: "cert.pem", key: "key.pem" })).to.have.property("cert", "cert.pem");
+      });
+
+      it("does not include a value for `key`", () => {
+        expect(config( { pfx: "apn.pfx", cert: "cert.pem", key: "key.pem" })).to.have.property("key", "key.pem");
+      });
+    });
+
+    context("pfxData value is supplied without cert and key", () => {
+      it("includes the value of `pfxData`", () => {
+        expect(config( { pfxData: "apnData" } )).to.have.property("pfxData", "apnData");
+      });
+
+      it("does not include a value for `cert`", () => {
+        expect(config( { pfxData: "apnData" } ).cert).to.be.undefined;
+      });
+
+      it("does not include a value for `key`", () => {
+        expect(config( { pfxData: "apnData" }).key).to.be.undefined;
+      });
+    });
+
+    context("pfxData value is supplied along with a cert and key", () => {
+      it("includes the value of `pfxData`", () => {
+        expect(config( { pfxData: "apnData", cert: "cert.pem", key: "key.pem" } )).to.have.property("pfxData", "apnData");
+      });
+
+      it("does not include a value for `cert`", () => {
+        expect(config( { pfxData: "apnData", cert: "cert.pem", key: "key.pem" })).to.have.property("cert", "cert.pem");
+      });
+
+      it("does not include a value for `key`", () => {
+        expect(config( { pfxData: "apnData", cert: "cert.pem", key: "key.pem" })).to.have.property("key", "key.pem");
+      });
+    });
+  });
+
+  context("a null config value is passed", () => {
+    it("should log a message with `debug`", () => {
+      config( { address: null } );
+
+      expect(fakes.debug).to.be.calledWith("Option [address] is null. This may cause unexpected behaviour.");
+    });
+  });
+
+  context("a config value is undefined", () => {
+    it("should log a message with `debug`", () => {
+      config( { anOption: undefined } );
+
+      expect(fakes.debug).to.be.calledWith("Option [anOption] is undefined. This may cause unexpected behaviour.");
+    });
+  });
+});
--- test/connection.js
+++ test/connection.js
@@ -19,53 +19,6 @@ describe("Connection", function() {
 	})
 
 	describe("constructor", function () {
-		let originalEnv;
-
-		before(function() {
-			originalEnv = process.env.NODE_ENV;
-		});
-
-		after(function() {
-			process.env.NODE_ENV = originalEnv;
-		});
-
-		beforeEach(function() {
-			process.env.NODE_ENV = "";
-		});
-
-		// Issue #50
-		it("should use api.sandbox.push.apple.com as the default connection address", function () {
-			expect(Connection().options.address).to.equal("api.sandbox.push.apple.com");
-		});
-
-		it("should use api.push.apple.com when NODE_ENV=production", function () {
-			process.env.NODE_ENV = "production";
-			expect(Connection().options.address).to.equal("api.push.apple.com");
-		});
-
-		it("should give precedence to production flag over NODE_ENV=production", function () {
-			process.env.NODE_ENV = "production";
-			expect(Connection({ production: false }).options.address).to.equal("api.sandbox.push.apple.com");
-		});
-
-		it("should use api.push.apple.com when production:true", function () {
-			expect(Connection({production:true}).options.address).to.equal("api.push.apple.com");
-		});
-
-		it("should use a custom address when passed", function () {
-			expect(Connection({address: "testaddress"}).options.address).to.equal("testaddress");
-		});
-
-		describe("address is passed", function() {
-			it("sets production to true when using production address", function() {
-				expect(Connection({address: "api.push.apple.com"}).options.production).to.be.true;
-			});
-
-			it("sets production to false when using sandbox address", function() {
-				process.env.NODE_ENV = "production";
-				expect(Connection({address: "api.sandbox.push.apple.com"}).options.production).to.be.false;
-			});
-		});
 
 		it("creates an EndpointManager", function() {
 			var options = { address: "api.push.apple.com" }
