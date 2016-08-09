6f5e14bcb6926e0caa7b703dd9fd6ea50f97c5e9
Andrew Naylor <argon@mkbot.net>
2016-08-09 21:27:46 +0100

    Remove token length constraints
    It was announced at WWDC 2015 (Session 720) that large device tokens are
    coming soon:
    
    > Let's look at what's ahead. Coming next year are large device tokens. Device
    > tokens today are 32 bytes.
    >
    > And next year, in 2016, they may be growing up to 100 bytes. So if you make
    > any assumptions about the size of device tokens in your code, or in your
    > server APIs, now is a good time to revisit those assumptions. Large device
    > tokens coming in 2016.
    
    Thanks for the reminder @maccheck

--- test/token.js
+++ test/token.js
@@ -23,14 +23,6 @@ describe("token", function () {
     it("throws when input is empty", function () {
       expect(function () { token(""); }).to.throw(/invalid length/);
     });
-
-    it("throws when input is too short", function () {
-      expect(function () { token("a9d0ed10e9cfd022a61cb08753f49c5a0b0dfb784697bf9f9d750a1003da19c") }).to.throw(/invalid length/);
-    });
-
-    it("throws when input is too long", function () {
-      expect(function () { token("a9d0ed10e9cfd022a61cb08753f49c5a0b0dfb784697bf9f9d750a1003da19c788") }).to.throw(/invalid length/);
-    });
   });
 
   context("Buffer input", function() {
@@ -43,16 +35,6 @@ describe("token", function () {
 
     it("throws when input is empty", function () {
       expect(function () { token(new Buffer([])); }).to.throw(/invalid length/);
-    });
-
-    it("throws when input is too short", function () {
-      const input = new Buffer("a9d0ed10e9cfd022a61cb08753f49c5a0b0dfb784697bf9f9d750a1003da19", "hex");
-      expect(function () { token(input) }).to.throw(/invalid length/);
-    });
-
-    it("throws when input is too long", function () {
-      const input = new Buffer("a9d0ed10e9cfd022a61cb08753f49c5a0b0dfb784697bf9f9d750a1003da19c788", "hex");
-      expect(function () { token(input) }).to.throw(/invalid length/);
     });
   });
 });
