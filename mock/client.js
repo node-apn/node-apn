7da462d7d21b4dbf286054d6be6a3e851fc775d4
Andrew Naylor <argon@mkbot.net>
2016-09-22 00:25:09 +0100

    Improve the default mock behaviour
    It previously just returned an empty object which isn't very useful.

--- mock/client.js
+++ mock/client.js
@@ -5,8 +5,8 @@ module.exports = function() {
   function Client() {
   }
 
-  Client.prototype.write = function mockWrite() {
-    return {};
+  Client.prototype.write = function mockWrite(notification, device) {
+    return { device };
   };
 
   return Client;
