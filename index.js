fc6e697e1683f703c1ce7e38b2eafdee798117d7
Florian Reinhart <florian.reinhart@gmail.com>
2017-12-05 17:24:59 +0100

    Use native node HTTP/2 client

--- index.js
+++ index.js
@@ -11,33 +11,11 @@ const config = require("./lib/config")({
   prepareCA: credentials.ca,
 });
 
-const tls = require("tls");
-const http = require("http");
-
-const framer     = require("http2/lib/protocol/framer");
-const compressor = require("http2/lib/protocol/compressor");
-
-const protocol = {
-  Serializer:   framer.Serializer,
-  Deserializer: framer.Deserializer,
-  Compressor:   compressor.Compressor,
-  Decompressor: compressor.Decompressor,
-  Connection:   require("http2/lib/protocol/connection").Connection,
-};
-
-const Endpoint = require("./lib/protocol/endpoint")({
-  tls,
-  http,
-  protocol,
-});
-
-const EndpointManager = require("./lib/protocol/endpointManager")({
-  Endpoint,
-});
+const http2 = require("http2");
 
 const Client = require("./lib/client")({
   config,
-  EndpointManager,
+  http2,
 });
 
 const Provider = require("./lib/provider")({
