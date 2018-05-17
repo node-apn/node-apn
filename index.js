72ec3de12f1f13440b9409ad553f8695955c63c5
bingomanatee <bingomanatee@me.com>
2018-05-16 17:03:48 -0700

    event warning fix

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const config = require("./lib/config")({
 const tls = require("tls");
 const http = require("http");
 
-const framer     = require("http2/lib/protocol/framer");
-const compressor = require("http2/lib/protocol/compressor");
+const framer     = require("./http2_mod/lib/protocol/framer");
+const compressor = require("./http2_mod/lib/protocol/compressor");
 
 const protocol = {
   Serializer:   framer.Serializer,
   Deserializer: framer.Deserializer,
   Compressor:   compressor.Compressor,
   Decompressor: compressor.Decompressor,
-  Connection:   require("http2/lib/protocol/connection").Connection,
+  Connection:   require("./http2_mod/lib/protocol/connection").Connection,
 };
 
 const Endpoint = require("./lib/protocol/endpoint")({
